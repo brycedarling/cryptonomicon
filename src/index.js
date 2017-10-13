e924cb9afeba3c7b1d9b56ed951f2392240cb236
Bryce Darling <bryce@brycedarling.com>
2017-10-12 20:02:45 -0400

    Remove old redux store code from index

--- src/index.js
+++ src/index.js
@@ -3,20 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
-import rootReducer, { actions } from './reducers';
-
-window.actions = actions;
-
-const store = createStore(rootReducer);
-
-window.store = store;
-
-store.subscribe(() => {
-  const state = store.getState();
-  console.log('data', state.ticks.data);
-  console.log('ui', state.ticks.ui.loading, state.ticks.ui.error);
-});
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
