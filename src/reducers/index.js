fad07226253ea70da4226affbf4fcf30865a6408
Bryce Darling <bryce@brycedarling.com>
2017-10-16 16:47:44 -0400

    Remove volumeChart reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import ticks from './ticks';
-import volumeChart from './volumeChart';
 
 export { actions } from './ticks';
 
 export default combineReducers({
   ticks,
-  volumeChart,
 });
